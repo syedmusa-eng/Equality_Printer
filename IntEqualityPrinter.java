56bfd14c18db0aee36b9d253f1a0619da015fa99
SYED MUSA RAZA <79834398+syedmusa-eng@users.noreply.github.com>
2021-05-07 07:54:43 +0200

    Running Program

---
+++ IntEqualityPrinter.java
@@ -0,0 +1,24 @@
+public class IntEqualityPrinter {
+
+    //Creating a method
+    public static void printEqual(int first, int second, int third) {
+
+        if (first < 0 || second < 0 || third < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            if (first == second &&
+                    first == third &&
+                    second == third) {
+                System.out.println("All numbers are equal");
+
+            } else if (first != second &&
+                    second != third &&
+                    first != third) {
+                System.out.println("All number are different");
+
+            } else {
+                System.out.println("Neither all are equal or different");
+            }
+        }
+    }
+}
